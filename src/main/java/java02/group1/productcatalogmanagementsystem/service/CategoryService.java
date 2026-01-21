026c0c84362123c909fffc0f3e8162ddd7233d8d
Le Duc Thinh <196206462+ThinhLe1108@users.noreply.github.com>
2026-01-21 20:11:09 +0700

    Create, Update, ViewAll, FindByName, FindById
    http://localhost:8080/api/categories/create
    input:
    {
    	"name": "Electrics",
    	"description": "blabla"
    }
    output:
    {
    	"description": "blabla",
    	"id": 1,
    	"name": "Electrics"
    }
    
    http://localhost:8080/api/categories/id/1
    input:
    {
    	"name": "blablabla"
    }
    output:
    {
    	"description": null,
    	"id": 1,
    	"name": "blablabla"
    }
    
    http://localhost:8080/api/categories
    output:
    [
    	{
    		"description": "blablabla",
    		"id": 1,
    		"name": "electrics"
    	}
    ]
    
    http://localhost:8080/api/categories/name/ele
    output:
    [
    	{
    		"description": "blablabla",
    		"id": 1,
    		"name": "electrics"
    	}
    ]
    
    http://localhost:8080/api/categories/id/1
    output:
    {
    	"description": "blablabla",
    	"id": 1,
    	"name": "electrics"
    }

---
+++ src/main/java/java02/group1/productcatalogmanagementsystem/service/CategoryService.java
@@ -0,0 +1,44 @@
+package java02.group1.productcatalogmanagementsystem.service;
+
+import java02.group1.productcatalogmanagementsystem.entity.Category;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java02.group1.productcatalogmanagementsystem.repository.CategoryRepository;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Transactional
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Transactional
+    public List<Category> getCategoriesByName(String name) {
+        return categoryRepository.findByNameContainingIgnoreCase(name);
+    }
+
+    @Transactional
+    public Category getById(Long id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+    }
+
+    @Transactional
+    public Category create(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    @Transactional
+    public Category update(Long id, Category updatedCategory) {
+        Category existingCategory = getById(id);
+        existingCategory.setName(updatedCategory.getName());
+        existingCategory.setDescription(updatedCategory.getDescription());
+        return categoryRepository.save(existingCategory);
+    }
+}
